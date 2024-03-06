f649b9ccccef0dad8fe376edcd89c2f1aac761b1
denizgorur <zfac242@live.rhul.ac.uk>
2024-03-06 00:03:33 +0000

    Argument.py now stores the argument that it represents abstractly.

--- src/uncertainpy/gradual/Argument.py
+++ src/uncertainpy/gradual/Argument.py
@@ -1,10 +1,12 @@
 class Argument:
-    def __init__(self, name, initial_weight, strength=None, attackers=None, supporters=None):
+    def __init__(self, name, arg, initial_weight, strength=None, attackers=None, supporters=None):
         self.name = name
+        self.arg = arg
         self.initial_weight = initial_weight
         self.strength = strength
         self.attackers = attackers
         self.supporters = supporters
+        self.parent = None
 
         if type(initial_weight) != int and type(initial_weight) != float:
             raise TypeError("initial_weight must be of type integer or float")
@@ -14,12 +16,15 @@ class Argument:
 
         if attackers is None:
             self.attackers = []
-        
+
         if supporters is None:
             self.supporters = []
 
     def get_name(self):
         return self.name
+
+    def get_arg(self):
+        return self.arg
 
     def add_attacker(self, attacker):
         self.attackers.append(attacker)
@@ -27,12 +32,15 @@ class Argument:
     def add_supporter(self, supporter):
         self.supporters.append(supporter)
 
+    def add_parent(self, parent):
+        self.parent = parent
+
     def get_initial_weight(self):
         return self.initial_weight
-        
+
     def reset_initial_weight(self, weight):
         self.initial_weight = weight
-        
+
     def __repr__(self) -> str:
         return f"Argument {self.name}: initial weight {self.initial_weight}, strength {self.strength}, attackers {self.attackers}, supporters {self.supporters}"
 
